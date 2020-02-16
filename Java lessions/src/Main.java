7cc10419b5158b90ba05eeb35b0919868e9a680c
a.kulmetev <a.kulmetev@cs-nb174204.domain.local>
2020-02-16 18:30:48 +0300

    3

--- Java lessions/src/Main.java
+++ Java lessions/src/Main.java
@@ -78,6 +78,16 @@ public class Main {
 		
 		System.out.println("Hello world!");
 		
+		System.out.println("Hello world!");
+		System.out.println("Hello world!");
+		System.out.println("Hello world!");
+		System.out.println("Hello world!");
+		System.out.println("Hello world!");
+		System.out.println("Hello world!");
+		System.out.println("Hello world!");
+		System.out.println("Hello world!");
+		System.out.println("Hello world!");
+		
 	}
 
 }
